664d671c37278de828f34ddd360c79c1e8fe0184
lousongtao <lousongtao@gmail.com>
2025-11-21 22:16:30 -0500

    Update DishNameTextView. When text is more, it can downgrade font size and auto 2 lines showing text.

--- app/src/main/java/com/shuishou/digitalmenu/ui/components/DishNameTextView.java
+++ app/src/main/java/com/shuishou/digitalmenu/ui/components/DishNameTextView.java
@@ -17,20 +17,4 @@ public class DishNameTextView extends ChangeLanguageTextView {
         super(context, attrs);
     }
 
-    public void show(byte language){
-        super.show(language);
-        if (getText().length() < 15) {
-            int bigfont = 20;
-            setTextSize(bigfont);
-            setMaxLines(1);
-        }else if (getText().length() < 25) {
-            int middlefont = 15;
-            setTextSize(middlefont);
-            setMaxLines(1);
-        }else {
-            int smallfont = 13;
-            setTextSize(smallfont);
-            setMaxLines(2);
-        }
-    }
 }
